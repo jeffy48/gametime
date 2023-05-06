a8559f5e81cc839b7e7ed5822ace1b90225f5148
Jeff <jeffyoon48@gmail.com>
2023-05-06 14:20:01 -0700

    setup and tested database, express, and server

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+app.use(morgan('dev')); // logs info about requests and responses
+app.use(cookieParser()); // parses cookies
+app.use(express.json()); // parses json bodies of requests w/ Content-Type of application/json
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+  }
+// helmet helps set a variety of headers to better secure your app
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
+);
+// Set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+);
+
+app.use(routes);
+
+module.exports = app;
