1836eae692fef8ac0a574ccd36a3aab208de4439
Jeff <jeffyoon48@gmail.com>
2023-05-16 20:40:00 -0700

    added model and migration for group table

---
+++ backend/db/migrations/20230517032903-create-group.js
@@ -0,0 +1,56 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Groups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      organizerId: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      about: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      private: {
+        type: Sequelize.BOOLEAN
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      previewImage: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Groups';
+    await queryInterface.dropTable(options);
+  }
+};
---
+++ backend/db/models/group.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Group extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Group.init({
+    id: DataTypes.INTEGER,
+    organizerId: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    about: DataTypes.STRING,
+    type: DataTypes.STRING,
+    private: DataTypes.BOOLEAN,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    previewImage: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Group',
+  });
+  return Group;
+};
