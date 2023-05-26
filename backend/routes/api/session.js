9d4401c23663d65f38cc17f4ca63d3c8f6299538
Jeff <jeffyoon48@gmail.com>
2023-05-25 20:37:18 -0700

    edited login endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -57,10 +57,8 @@ router.post(
         });
 
         if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-            const err = new Error('Login failed');
+            const err = new Error('Invalid credentials');
             err.status = 401;
-            err.title = 'Login failed';
-            err.errors = { credential: 'The provided credentials were invalid.' };
             return next(err);
         };
 
