8940cb12420ae6ffc0373a93a12094d5b1ba8366
Jeff <jeffyoon48@gmail.com>
2023-07-26 21:33:43 -0700

    added logout thunk action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -56,6 +56,14 @@ export const signup = (user) => async (dispatch) => {
     return response;
 };
 
+export const logout = (user) => async dispatch => {
+    const res = await csrfFetch('/api/session', {
+        method: 'DELETE'
+    });
+    dispatch(removeUser());
+    return res;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
